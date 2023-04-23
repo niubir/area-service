2e9edd60e7fbe021b99f9efa1619f18775bfaf5f
zhanyangyang <zhanyangyang@yungengxin.com>
2023-04-23 17:04:25 +0800

    update build.sh

--- helper/docker.sh
+++ helper/docker.sh
@@ -2,4 +2,6 @@ docker build -t niubir/area-service:test .
 
 docker run -p 10011:10011 -p 10012:10012 -v /root/area-service/config:/config -v /root/area-service/data/:/data -v /root/area-service/logs/:/logs --name=area-service-1 -d niubir/area-service:test
 
+docker push niubir/area-service:test
+
 docker inspect --format='{{json .Mounts}}' area-service-1
